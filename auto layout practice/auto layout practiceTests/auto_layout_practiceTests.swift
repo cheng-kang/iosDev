ab1fcf7cc7c57f0c5d050a2f3d6df7eb53b21855
Ant <me@chengkang.pw>
2016-03-10 04:32:55 +0000

    auto layout first try

---
+++ auto layout practice/auto layout practiceTests/auto_layout_practiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_layout_practiceTests.swift
+//  auto layout practiceTests
+//
+//  Created by Ant on 3/10/16.
+//  Copyright © 2016 Ant. All rights reserved.
+//
+
+import XCTest
+@testable import auto_layout_practice
+
+class auto_layout_practiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
