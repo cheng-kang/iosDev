e08cefba35e2d7429c19d17c3f4d1afda946979f
Ant <me@chengkang.pw>
2016-03-11 21:37:44 +0000

    update
    init my rock pet

---
+++ My Rock Pet/My Rock Pet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Rock Pet
+//
+//  Created by Ant on 3/11/16.
+//  Copyright © 2016 Ant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
