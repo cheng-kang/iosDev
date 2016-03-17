74f2a9ab0151628b526940d4374a39f825926635
Ant <me@chengkang.pw>
2016-03-17 13:43:06 +0000

    PokeDex

---
+++ PokeDex/PokeDex/PokeCell.swift
@@ -0,0 +1,16 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Ant on 3/16/16.
+//  Copyright © 2016 Ant. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    func configureCell() {
+        
+    }
+}
