bc43fa84558af29a4b4e7cd69a17ab5b580ef127
cheng-kang <me@chengkang.pw>
2016-03-19 02:44:23 +0000

    update
    Start to write something like Weibo App

---
+++ Weibo/Weibo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Weibo
+//
+//  Created by Ant on 3/19/16.
+//  Copyright © 2016 Ant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
