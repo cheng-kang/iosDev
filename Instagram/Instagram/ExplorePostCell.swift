b2b2609c7e8cd4192773cc8170d6950ef136b701
cheng-kang <me@chengkang.pw>
2016-04-06 20:37:55 +0100

    update

---
+++ Instagram/Instagram/ExplorePostCell.swift
@@ -0,0 +1,22 @@
+//
+//  ExplorePostCell.swift
+//  Instagram
+//
+//  Created by Ant on 4/6/16.
+//  Copyright © 2016 Ant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExplorePostCell: UICollectionViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func initCell(image: UIImage!) {
+        self.img.image = image
+    }
+}
--- Instagram/Instagram/SecondViewController.swift
+++ Instagram/Instagram/SecondViewController.swift
@@ -8,19 +8,45 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var explorePosts: [UIImage] = [UIImage(named: "test")!, UIImage(named: "test")!, UIImage(named: "test")!, UIImage(named: "test")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print(self.tabBarItem.image?.size)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 1
+        layout.minimumLineSpacing = 1
+        
+        collectionView.collectionViewLayout = layout
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
     }
-
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ExplorePostCell", forIndexPath: indexPath) as! ExplorePostCell
+        cell.initCell(self.explorePosts[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.explorePosts.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake((self.view.frame.width - 2) / 3, (self.view.frame.width - 2) / 3)
+    }
 
 }
 
