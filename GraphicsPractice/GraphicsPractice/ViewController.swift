27f75f2e98afb4a03bd2df2cb88e04b1189bbcc6
cheng-kang <me@chengkang.pw>
2016-05-19 09:36:26 +0800

    update

--- GraphicsPractice/GraphicsPractice/ViewController.swift
+++ GraphicsPractice/GraphicsPractice/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        filterTest()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func filterTest() {
+        
+        let context = CIContext()
+        let cImage = CIImage(CGImage: self.image.image!.CGImage!)
+        
+        let gaussianBlur = CIFilter(name: "CIGaussianBlur")
+        gaussianBlur?.setValue(cImage, forKey: "inputImage")
+        gaussianBlur?.setValue(1, forKey: "inputRadius")
+        var result = gaussianBlur?.valueForKey("outputImage") as! CIImage
+        
+        let imageRef = context.createCGImage(result, fromRect: CGRectMake(100, 20, self.image.frame.width, self.image.frame.height))
+        
+        let image = UIImage(CIImage: result)
+        self.image.image = image
     }
-
 
 }
 
