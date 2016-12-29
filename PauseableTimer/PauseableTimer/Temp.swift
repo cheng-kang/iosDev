d11567b4489ffc6c5f89d71a2d6787c5464ca545
Kang Cheng <me@chengkang.pw>
2016-12-29 16:00:18 +0800

    PauseableTimer Done

--- PauseableTimer/PauseableTimer/Temp.swift
+++ PauseableTimer/PauseableTimer/Temp.swift
@@ -1,5 +1,6 @@
+
 //
-//  Temp.swift
+//  PauseableTimer.swift
 //  PauseableTimer
 //
 //  Created by Ant on 28/12/2016.
@@ -8,10 +9,29 @@
 
 import Foundation
 
-class Temp: NSObject {
+enum PauseableTimerInitType: Int {
+    case timeIntervalWithSelector = 1
+    case timeIntervalWithBlock = 2
+    case firedateWithSelector = 3
+    case firedateWithBlock = 4
+}
+
+class PauseableTimer1: NSObject {
     var timer: Timer!
-    var startTime: TimeInterval!
-    var stopTime: TimeInterval!
+    var startTime: TimeInterval?
+    var stopTime: TimeInterval?
+    
+    private var type: PauseableTimerInitType!
+    
+    private var _firedate: Date?
+    private var _timeInterval: TimeInterval?
+    private var _target: Any?
+    private var _selector: Selector?
+    private var _userInfo: Any?
+    private var _repeats: Bool?
+    private var _block: ((Timer) -> Swift.Void)?
+    
+    private(set) var isPause: Bool = false
     
     var isValid: Bool {
         return self.timer.isValid
@@ -30,22 +50,120 @@ class Temp: NSObject {
         return self.timer.tolerance
     }
     
-    init(timer aTimer: Timer) {
-        self.startTime = NSDate.timeIntervalSinceReferenceDate
-        self.timer = aTimer
+    init(timeInterval ti: TimeInterval, target aTarget: Any, selector aSelector: Selector, userInfo: Any?, repeats yesOrNo: Bool) {
+        self.type = PauseableTimerInitType.timeIntervalWithSelector
+        
+        self._timeInterval = ti
+        self._target = aTarget
+        self._selector = aSelector
+        self._userInfo = userInfo
+        self._repeats = yesOrNo
+        
+        self.timer = Timer(timeInterval: ti, target: aTarget, selector: aSelector, userInfo: userInfo, repeats: yesOrNo)
+        self.startTime = Date().timeIntervalSinceReferenceDate
+        
+        RunLoop.current.add(self.timer, forMode: .commonModes)
+    }
+    
+    @available(iOS 10.0, *)
+    init(timeInterval interval: TimeInterval, repeats: Bool, block: @escaping (Timer) -> Swift.Void) {
+        self.type = PauseableTimerInitType.timeIntervalWithBlock
+        
+        self._timeInterval = interval
+        self._repeats = repeats
+        self._block = block
+        
+        self.timer = Timer.init(timeInterval: interval, repeats: repeats, block: block)
+        self.startTime = Date().timeIntervalSinceReferenceDate
+        
+        RunLoop.current.add(self.timer, forMode: .commonModes)
+    }
+    
+    init(fireAt date: Date, interval ti: TimeInterval, target t: Any, selector s: Selector, userInfo ui: Any?, repeats rep: Bool) {
+        self.type = PauseableTimerInitType.firedateWithSelector
+        
+        self._firedate = date
+        self._timeInterval = ti
+        self._target = t
+        self._selector = s
+        self._userInfo = ui
+        self._repeats = rep
+        
+        self.timer = Timer(fireAt: date, interval: ti, target: t, selector: s, userInfo: ui, repeats: rep)
+        self.startTime = Date().timeIntervalSinceReferenceDate
+        
+        RunLoop.current.add(self.timer, forMode: .commonModes)
+    }
+    
+    @available(iOS 10.0, *)
+    init(fire date: Date, interval: TimeInterval, repeats: Bool, block: @escaping (Timer) -> Swift.Void) {
+        self.type = PauseableTimerInitType.firedateWithBlock
+        
+        self._firedate = date
+        self._timeInterval = interval
+        self._repeats = repeats
+        self._block = block
+        
+        self.timer = Timer(fire: date, interval: interval, repeats: repeats, block: block)
+        self.startTime = Date().timeIntervalSinceReferenceDate
+        
+        RunLoop.current.add(self.timer, forMode: .commonModes)
     }
     
     func invalidate() {
         self.stopTime = NSDate.timeIntervalSinceReferenceDate
         self.timer.invalidate()
+        self.resumeTimer?.invalidate()
+        self.isPause = false
     }
     
     func pause() {
-        self.invalidate()
+        if !isPause {
+            self.invalidate()
+            isPause = true
+        }
     }
     
+    var lastTimeLeft: TimeInterval?
+    var resumeTimer: Timer?
     func resume() {
-        let timeInterval = self.timer.timeInterval
-        let fireDate = self.timer.fireDate
+        if isPause {
+            switch self.type! {
+            case .timeIntervalWithSelector:
+                let timePassed = stopTime! - startTime!
+                let timeLeft = self._timeInterval! - timePassed.truncatingRemainder(dividingBy: self._timeInterval!)
+                // check if the Timer is expired
+                if timeLeft >= 0.08 {
+                    // not expired
+                    
+                    // renew timer
+                    self.timer = Timer(timeInterval: self._timeInterval!, target: self._target!, selector: self._selector!, userInfo: self._userInfo, repeats: self._repeats!)
+                    // schedule next fire
+                    resumeTimer = Timer.scheduledTimer(withTimeInterval: timeLeft, repeats: false, block: { (_) in
+                        self.timer.fire()
+                        if self._repeats! {
+                            RunLoop.current.add(self.timer, forMode: .commonModes)
+                        } else {
+                            self.timer.invalidate()
+                        }
+                        self.resumeTimer = nil
+                    })
+                    lastTimeLeft = timeLeft
+                } else {
+                    // if self._repeats! && timeLeft == 0 {
+                    //     self.timer = Timer(timeInterval: self._timeInterval!, target: self._target!, selector: self._selector!, userInfo: self._userInfo!, repeats: self._repeats!)
+                    //     RunLoop.current.add(self.timer, forMode: .commonModes)
+                    //     self.timer.fire()
+                    // }
+                }
+            case .timeIntervalWithBlock:
+                break
+            case .firedateWithSelector:
+                break
+            case .firedateWithBlock:
+                break
+            }
+            isPause = false
+        }
     }
 }
--- PauseableTimer/PauseableTimer/ViewController.swift
+++ PauseableTimer/PauseableTimer/ViewController.swift
@@ -10,24 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textview: UITextView!
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    @IBOutlet weak var lbl3: UILabel!
+    
+    var timerNeverStop: Timer!
+    var timerWillPause: PauseableTimer1!
+    var time: Double = 0
+    
+    var time1: Double = 0
+    var time2: Double = 0
+    var time3: Double = 0
+    
+    @IBAction func startBtnClick() {
+        timerWillPause = PauseableTimer1(timeInterval: 3, target: self, selector: #selector(ViewController.test), userInfo: nil, repeats: true)
+        self.textview.text = self.textview.text+"Start at: \(Date())\n\(Date().timeIntervalSinceReferenceDate)\ntime = \(self.time)"
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timer in
+            self.time1 += 0.1
+            self.lbl1.text = "\(self.time1)"
+        })
+        Timer.scheduledTimer(withTimeInterval: 0.048, repeats: true, block: { timer in
+            self.time2 += 0.048
+            self.lbl2.text = "\(self.time2)"
+        })
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { timer in
+            self.time3 += 0.05
+            self.lbl3.text = "\(self.time3)"
+        })
+    }
+    
+    @IBAction func pauseBtnClick() {
+        timerWillPause.pause()
+        self.textview.text = self.textview.text+"\nPause at: \(Date())\n\(Date().timeIntervalSinceReferenceDate)\ntime = \(self.time)"
+    }
+    
+    @IBAction func resumeBtnClick() {
+        timerWillPause.resume()
+        self.textview.text = self.textview.text+"\nResume at: \(Date())\n\(Date().timeIntervalSinceReferenceDate)\ntime = \(self.time)"
+    }
+    
+    func test() {
+        self.time += 3
+        self.textview.text = self.textview.text+"Triger at: \(Date())\n\(Date().timeIntervalSinceReferenceDate)\ntime = \(self.time)"
+    }
+    
     fileprivate var thingWatchingRunLoop = RunLoop.current
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+    }
+    
+    func testTimers() {
+        
         print(Date())
         
         Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timer1(timer:)), userInfo: nil, repeats: true)
-        let timer2 = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+        var timer2: Timer? = nil
+        timer2 = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
             print("🌻Timer2")
             print(Date())
             print(timer.fireDate)
             print(timer.isValid)
             print(timer.timeInterval)
             print(timer.tolerance)
+            print(timer2)
+            timer2 = nil
+            print(timer2)
         })
-        timer2.fire()
+        timer2?.fire()
         print("timer2 fire")
         
         let timer3 = Timer(fire: Date() , interval: 1, repeats: true, block: { timer in
@@ -52,7 +106,7 @@ class ViewController: UIViewController {
         })
         timer4.fire()
         print("timer4 fire")
-        print(timer4 == nil)
+        print(timer4.isValid)
         
         let timer5 = Timer(timeInterval: 1, repeats: true, block: { timer in
             print("🔥Timer5")
@@ -63,6 +117,7 @@ class ViewController: UIViewController {
             print(timer.tolerance)
         })
         
+        // Timer that manually fired and not added to RunLoop
         let timer6 = Timer(timeInterval: 1, repeats: true, block: { timer in
             print("🔥Timer6")
             print(Date())
@@ -71,7 +126,7 @@ class ViewController: UIViewController {
             print(timer.timeInterval)
             print(timer.tolerance)
         })
-        timer6.fire()
+        timer6.fire() // Result: Fire once
         print("timer6 fire")
         
         let timer7 = Timer(fire: Date().addingTimeInterval(TimeInterval(3)), interval: 1, repeats: false, block: { timer in
@@ -81,6 +136,10 @@ class ViewController: UIViewController {
             print(timer.isValid)
             print(timer.timeInterval)
             print(timer.tolerance)
+            timer.invalidate()
+            print(timer.isValid)
+            timer.invalidate()
+            print(timer.isValid)
         })
         thingWatchingRunLoop.add(timer7, forMode: .commonModes)
         print(Date())
@@ -95,18 +154,79 @@ class ViewController: UIViewController {
             print(timer.tolerance)
         })
         
+        let timer9 = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { timer in
+            print("💦Timer9")
+            print(Date())
+            print(timer.fireDate)
+            print(timer.isValid)
+            print(timer.timeInterval)
+            print(timer.tolerance)
+            //timer.invalidate()
+            //print("Incalidated")
+            //print(timer.isValid)
+            //print("Fire")
+            //timer.fire()
+            
+            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false, block: { (t) in
+                
+                print("timer9 after 0.1 sec")
+                print(timer.isValid)
+                print(timer.fireDate)
+            })
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { (t) in
+                
+                print("timer9 after 1 sec")
+                print(timer.isValid)
+                print(timer.fireDate)
+            })
+        })
+        
+        // Timer that has a expired firedate
+        let timer10 = Timer(fire: Date().addingTimeInterval(TimeInterval(-5)), interval: 1, repeats: false, block: { (timer) in
+            print("💦Timer10")
+            print(Date())
+            print(timer.fireDate)
+            print(timer.isValid)
+            print(timer.timeInterval)
+            print(timer.tolerance)
+        })
+        thingWatchingRunLoop.add(timer10, forMode: .commonModes)
+        print(Date())
+        print("timer10 added to runloop")
+        
+        let timer11 = Timer(timeInterval: 1, repeats: true, block: { timer in
+            print("💦Timer11")
+            print(Date())
+            print(timer.fireDate)
+            print(timer.isValid)
+            print(timer.timeInterval)
+            print(timer.tolerance)
+        })
+        
         Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { _ in
             self.thingWatchingRunLoop.add(timer8, forMode: .commonModes)
             print(Date())
             print("timer8 added to runloop")
-            timer2.invalidate()
+            timer2?.invalidate()
             print(Date())
             print("timer2 invalidated")
+            
+            print("timer6 info:")
+            print(timer6.isValid)
+            print(timer6.fireDate)
+            
+            print("timer9 info:")
+            print(timer9.isValid)
+            print(timer9.fireDate)
+            
+            print("timer10 info:")
+            print(timer10.isValid)
+            print(timer10.fireDate)
         })
     }
     
     func timer1(timer: Timer) {
-        print("🌺💦Timer1")
+        print("🌺Timer1")
         print(Date())
         print(timer.fireDate)
         print(timer.isValid)
